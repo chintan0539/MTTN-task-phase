92f8914fccc85742efe5993d1d58752133571cd3
Chintan0539 <chintanpatel0539@gmail.com>
2021-11-27 23:25:42 +0530

    data added

--- src/components/card/data.js
+++ src/components/card/data.js
@@ -35,16 +35,161 @@ const data = [
     sem: 2,
     department: "CSE",
   },
+
   {
-    title: "File 2 CSE S2",
+    title: "File 1 CSE s3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "CSE",
+  },
+  {
+    title: "File 2 CSE s3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "CSE",
+  },
+  {
+    title: "File 3 CSE s3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "CSE",
+  },
+  {
+    title: "File 1 CSE s4",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 4,
+    department: "CSE",
+  },
+  {
+    title: "File 2 CSE S4",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 4,
+    department: "CSE",
+  },
+  {
+    title: "File 3 CSE S5",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 5,
+    department: "CSE",
+  },
+
+  {
+    title: "File 3 CSE s5",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 5,
+    department: "CSE",
+  },
+  {
+    title: "File 1 CSE s6",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 6,
+    department: "CSE",
+  },
+  {
+    title: "File 2 CSE S6",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 6,
+    department: "CSE",
+  },
+
+  {
+    title: "File 1 CSE S1",
     link: "http://www.africau.edu/images/default/sample.pdf",
     sem: 1,
     department: "EEE",
   },
   {
-    title: "File 3 CSE S3",
+    title: "File 2 CSE S1",
     link: "http://www.africau.edu/images/default/sample.pdf",
     sem: 1,
+    department: "EEE",
+  },
+  {
+    title: "File 3 CSE S1",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 1,
+    department: "EEE",
+  },
+  {
+    title: "File 1 CSE S2",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 2,
+    department: "EEE",
+  },
+  {
+    title: "File 2 CSE S2",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 2,
+    department: "EEE",
+  },
+  {
+    title: "File 3 CSE S2",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 2,
+    department: "EEE",
+  },
+
+  {
+    title: "File 1 CSE S3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "EEE",
+  },
+  {
+    title: "File 2 CSE S3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "EEE",
+  },
+  {
+    title: "File 3 CSE S3",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 3,
+    department: "EEE",
+  },
+
+  {
+    title: "File 1 CSE S4",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 4,
+    department: "EEE",
+  },
+  {
+    title: "File 2 CSE S4",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 4,
+    department: "EEE",
+  },
+  {
+    title: "File 3 CSE S4",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 4,
+    department: "EEE",
+  },
+
+  {
+    title: "File 1 CSE S5",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 5,
+    department: "EEE",
+  },
+
+  {
+    title: "File 2 CSE S5",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 5,
+    department: "EEE",
+  },
+  {
+    title: "File 1 CSE S6",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 6,
+    department: "EEE",
+  },
+  {
+    title: "File 2 CSE S6",
+    link: "http://www.africau.edu/images/default/sample.pdf",
+    sem: 6,
     department: "EEE",
   },
 ];
